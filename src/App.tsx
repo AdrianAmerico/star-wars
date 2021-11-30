41ed5ae50f2a85b8add88a81e78f0956bf3e9775
AdrianAmerico <1fnadrian1@gmail.com>
2021-11-30 14:58:00 -0300

    adicionado roteamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import { GlobalContextProvider } from './app/context'
+import Router from './app/pages/Router'
 import { defaultTheme, GlobalStyle } from './atomic'
 
 const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalContextProvider>
-        <h1>Hello World</h1>
+        <Router/>
         <GlobalStyle />
       </GlobalContextProvider>
     </ThemeProvider>
