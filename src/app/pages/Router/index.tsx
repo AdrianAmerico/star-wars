fef702b6e43c2e78d21c24781e9abc53d274bdc0
AdrianAmerico <1fnadrian1@gmail.com>
2021-12-01 19:22:23 -0300

    mui-datatables esboço

--- src/app/pages/Router/index.tsx
+++ src/app/pages/Router/index.tsx
@@ -3,12 +3,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { routes } from '../../../atomic'
 import { Homepage } from '../Homepage'
+import { PlanetPage } from '../PlanetPage'
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={routes.base} element={<Homepage />} />
+        <Route path={routes.planet} element={<PlanetPage />} />
+
       </Routes>
     </BrowserRouter>
   )
