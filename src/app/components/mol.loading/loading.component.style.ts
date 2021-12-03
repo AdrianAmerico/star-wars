a7223ff0786bc4f1f44d11c248730aa7a1e90fb5
AdrianAmerico <1fnadrian1@gmail.com>
2021-12-03 00:10:04 -0300

    loading

---
+++ src/app/components/mol.loading/loading.component.style.ts
@@ -0,0 +1,10 @@
+import { Theme } from '@emotion/react'
+
+export const styles: Theme = {
+  display: 'flex',
+  position: 'absolute',
+  inset: '0',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center'
+}
--- src/app/components/mol.loading/loading.component.tsx
+++ src/app/components/mol.loading/loading.component.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import CircularProgress from '@mui/material/CircularProgress'
+import Box from '@mui/material/Box'
+import { styles } from './loading.component.style'
 
 export const Loading = () => {
-  return <h1>Carregando...</h1>
+  return (
+    <Box sx={styles}>
+      <CircularProgress size={100} />
+    </Box>
+  )
 }
